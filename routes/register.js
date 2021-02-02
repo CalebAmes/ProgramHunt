00ed5ae5936320ac595bd009abe9f0a30784e3d9
Caleb <caleb-gilbert@live.com>
2021-02-01 19:37:27 -0500

    end of day

--- routes/register.js
+++ routes/register.js
@@ -3,31 +3,32 @@ const express = require('express');
 const router = express.Router();
 const { users } = require('../db/models');
 const bcrypt = require('bcryptjs');
-const { check, validationResult } = require('express-validator')
+const check = require('express-validator/check')
+// const validationResult = require('express-validator/validationResult')
 const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
 
-const registationValidator = [
-  check('email')
-    .exist({checkFalsey: true})
-    .isEmail()
-    .withMessage = "Please provide a valid email address"
-    .custom((value) => {
-      return db.User.findOne({ where: { emailAddress: value } })
-        .then((user) => {
-          if (user) {
-            return Promise.reject('The provided Email Address is already in use by another account');
-          }
-        });
-    }),
+const registrationValidator = [
+  // .custom((email) => {
+    //   return db.users.findOne({ where: { emailAddress: email } })
+    //     .then((users) => {
+      //       if (users) {
+        //         return Promise.reject('The provided Email Address is already in use by another account');
+        //       }
+        //     });
+        // }),
   check('username')
-    .exist({checkFalsey: true})
+    .exists({checkFalsey: true})
     .isLength({max: 25})
     .withMessage = "This username is already in use",
+  check('email')
+    .exists({checkFalsey: true})
+    .isEmail()
+    .withMessage = "Please provide a valid email address",
   check('password')
-    .exist({checkFalsey: true})
+    .exists({checkFalsey: true})
     .withMessage = "Please provide a password",
   check('confirmPassword')
-    .exist({checkFalsey: true})
+    .exists({checkFalsey: true})
     .withMessage = "Please confirm your password"
 ]
 
@@ -36,11 +37,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', registrationValidator, asyncHandler(async(req, res, next) => {
-  const { username, email, password, confirmPassword } = req.body;
-  if(password !== confirmPassword) next();
+  const { username, email, password } = req.body;
 
   const hashedPassword = await bcrypt.hash(password, 8);
   const user = await users.create({ username, email, hashedPassword })
+  const validatorErrors = validationResult(req)
   res.redirect('/')
 }))
 
