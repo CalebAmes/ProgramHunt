9a0e1f887aee116ccd1ea85ce02bda31d1d7e29e
tyler <tylerruby0314@yahoo.com>
2021-02-04 14:56:31 -0600

    Changes to the migrations and models

--- db/models/program.js
+++ db/models/program.js
@@ -2,13 +2,16 @@
 module.exports = (sequelize, DataTypes) => {
   const Program = sequelize.define('Program', {
     name: DataTypes.STRING,
+    bio: DataTypes.STRING,
     description: DataTypes.TEXT,
     userId: DataTypes.INTEGER,
     video: DataTypes.STRING,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    likes: DataTypes.INTEGER
   }, {});
   Program.associate = function(models) {
     Program.belongsTo(models.User, { foreignKey: 'userId' });
+    Program.hasMany(models.Comment, { foreignKey: 'programId' })
   };
   return Program;
 };
