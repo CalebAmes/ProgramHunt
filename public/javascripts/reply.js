51157d132c930f459a3314f3f9ada517cc239222
tyler <tylerruby0314@yahoo.com>
2021-07-20 16:31:54 -0500

    Still getting issues with visibility, taking a break.

--- public/javascripts/reply.js
+++ public/javascripts/reply.js
@@ -1,12 +1,13 @@
 window.addEventListener("load", (event)=>{
-  let visibility = false
+  // let visibility = false
   const reply = document.getElementById('reply')
     reply.addEventListener('click', () => {
-      if (visibility === false){
-        visibility= true
-      }else{
-        visibility=false
-      }
+      document.getElementById('replyInputBox').setAttribute('visibility', 'visible')
+      // if (visibility === false){
+      //   visibility= true
+      // }else{
+      //   visibility=false
+      // }
   })
 
 })
